5f16f944ea60e38ed8598b3e222499ad7603bfcd
Patrickhuiwork <patrick.hui.work@gmail.com>
2023-12-07 21:27:19 -0800

    padding

--- src/app/components/atoms/tab/tab-container.tsx
+++ src/app/components/atoms/tab/tab-container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function TabContainer(props: {children:ReactNode}) {
     return (
-        <div className="bg-gray-100 rounded-xl">
+        <div className="bg-gray-100 rounded-xl pb-10">
             {props.children}
         </div>
     )
